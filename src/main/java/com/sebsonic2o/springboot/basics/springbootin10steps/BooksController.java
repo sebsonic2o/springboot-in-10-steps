213d8c7c7a1410415953ef8699058f3eb07ed14c
Sebastien Cheraux <scheraux@gmail.com>
2020-12-30 12:38:31 -0800

    add spring boot devtools to automatically and quickly restart app on change

--- src/main/java/com/sebsonic2o/springboot/basics/springbootin10steps/BooksController.java
+++ src/main/java/com/sebsonic2o/springboot/basics/springbootin10steps/BooksController.java
@@ -11,6 +11,6 @@ public class BooksController {
 
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
-		return Arrays.asList(new Book(1l, "Mastering Spring 5.0", "Ranga Karanam"));
+		return Arrays.asList(new Book(1l, "Mastering Spring 5.1", "Ranga Karanam"));
 	}
 }
